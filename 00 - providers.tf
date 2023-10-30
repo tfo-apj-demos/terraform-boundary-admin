454a3b3aeb5a0d69eecff3fc556dd62bfe465774
Grant Orchard <go@hashicorp.com>
2023-10-30 20:06:41 +1100

    configure oidc auth method in demo scope

--- 00 - providers.tf
+++ 00 - providers.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
     tfe = {
-      source = "hashicorp/tfe"
+      source  = "hashicorp/tfe"
       version = "~> 0"
     }
     boundary = {
-      source = "hashicorp/boundary"
-			version = "~> 1"
+      source  = "hashicorp/boundary"
+      version = "~> 1"
     }
   }
 }
 
 provider "boundary" {
-  addr = var.boundary_addr
+  addr                   = var.boundary_addr
   auth_method_login_name = var.boundary_username
-  auth_method_password = var.boundary_password
-  auth_method_id = var.boundary_auth_method_id
+  auth_method_password   = var.boundary_password
+  auth_method_id         = var.boundary_auth_method_id
 }
 
 provider "tfe" {
--- 11 - auth_methods.tf
+++ 11 - auth_methods.tf
@@ -1,21 +1,21 @@
 locals {
-  issuer = data.tfe_outputs.vault_admin.values.oidc_issuer
-  client_id = data.tfe_outputs.vault_admin.values.oidc_client_id
+  issuer        = data.tfe_outputs.vault_admin.values.oidc_issuer
+  client_id     = data.tfe_outputs.vault_admin.values.oidc_client_id
   client_secret = data.tfe_outputs.vault_admin.values.oidc_client_secret
 }
 
 resource "boundary_auth_method_oidc" "team_se" {
-  scope_id = boundary_scpope.tfo_apj_demo.id
-  issuer = local.issuer
-  client_id = local.client_id
-  client_secret = local.client_secret
+  scope_id           = boundary_scope.tfo_apj_demo.id
+  issuer             = local.issuer
+  client_id          = local.client_id
+  client_secret      = local.client_secret
   signing_algorithms = ["RS256"]
-  api_url_prefix = "https://e0078f32-6a05-44fe-a147-158f9d15a5fb.boundary.hashicorp.cloud"
+  api_url_prefix     = "https://e0078f32-6a05-44fe-a147-158f9d15a5fb.boundary.hashicorp.cloud"
   claims_scopes = [
     "groups",
     "user"
   ]
-  name = "vault"
+  name  = "vault"
   state = "inactive"
 }
 
